3143822f82271b36f86379d689bf43a4d4fd01f9
Deklerk Basson <216183006@mycput.ac.za>
2019-10-16 22:40:07 +0200

    Database and Interface for Employee Only

---
+++ src/main/java/za/ac/cput/repository/users/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package za.ac.cput.repository.users;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.domain.users.Employee;
+
+
+@Repository
+public interface EmployeeRepository 
+			extends CrudRepository<Employee, Long> {
+
+}
---
+++ target/classes/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO 
+	TBL_EMPLOYEES (first_name, last_name, email) 
+VALUES
+  	('Deklerk', 'Basson', '216183006'),
+  	('Test1', 'TestSurname1', '1'),
+  	('Test2', 'TestSurname2', '2'),
+  	('Test3', 'TestSurname3', '3'),
+  	('Test4', 'TestSurname4', '4'),
+  	('John', 'Hohn', 'email@email.com');
